0b583d31b214934d557fc8b8d37598a95d6cd09e
Mario <mario.junior2@alunos.sc.senac.br>
2025-03-01 19:21:04 -0300

    muito legal java

--- JAVA/estudos/Estudo001.java
+++ JAVA/estudos/Estudo001.java
@@ -2,18 +2,22 @@ import java.util.Scanner;
 
 public class Estudo001{
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.err.println("Digite seu nomezinho por favor!");
-        String nome = scan.nextLine();
-        System.err.println("Digite sua idade!");
-        int idade = scan.nextInt();
+        Scanner scan = new Scanner(System.in); // O input do java mas ele precisar ser aberto e fechado
+        System.err.println("Digite seu nomezinho por favor!"); // Print do JAVA
+        String nome = scan.nextLine(); // O input ainda aberto identificando para ler a proxima coisa a ser escrita mas em STR claro
+        System.err.println("Digite sua idade!"); // Print
+        int idade = scan.nextInt(); // Aqui o input ainda está aberto e vai ler o proximo numero que for digitado
 
       
-        System.err.printf("%s tem de idade %d ", nome, idade);
-        scan.close();
-        if (idade == 10) {
+        System.err.printf("%s tem de idade %d", nome, idade); // Aqui esta o prin Format que coloca as variaveis fora e dentro
+        // %s seria para String
+        // %d seria para Int
+        scan.close(); // E aqui fechamos o input
+        if (idade == 10) { // if padrao
             System.err.println(", Legal!");
-        } else {
+        } else if (idade == 18) { // elseif mas separado
+            System.err.println(", Putz lhe falta emprego kk");
+        } else { // else padrao
             System.err.println(", Nada legal AGORA ahahahahah!");
         }
 
